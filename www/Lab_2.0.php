93694ce7f4adbf1d1b684170eb62912cb9c18f68
Николай Эпаев <dvd11122@mail.ru>
2021-02-21 16:31:24 +0300

    start working on lab_2

---
+++ www/Lab_2.0.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <link rel='stylesheet' href='style_2.css'>
+  </head>
+  <body>
+    <table>
+      <tr>
+        <td>
+          <h2>Работа с циклическими структурами</h2>
+          <form>
+            <p>
+              <select name="param" size='1' id="">
+                <option selected value="s1">Таблица умножения</option>
+                <option value="s2">Сумма нечетных</option>
+                <option value="s3">Переводчик</option>
+              </select>
+            </p>
+            <input type="submit" value='Ок'>
+          </form>
+        </td>
+        <td>
+          <h2>Работа с файлми</h2>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
