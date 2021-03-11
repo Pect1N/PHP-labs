913f8e4b95c975f3d3a8f5b0440c063d6839d679
Николай Эпаев <dvd11122@mail.ru>
2021-03-11 03:49:43 +0300

    added work with sended massive and set color, added from message

--- www/Lab_2.2.php
+++ www/Lab_2.2.php
@@ -2,16 +2,42 @@
   <head>
   </head>
   <body>
-  <form action="http://myfirstsite.ru/Lab_2.1.php">
-		<input type="submit" value="Back">
-	</form>
+    <form action="http://myfirstsite.ru/Lab_2.1.php">
+      <input type="submit" value="Back">
+    </form>
     <p>Дата создания извещения: <font color='red'> <?php echo htmlspecialchars($_POST['Дата']); ?> </font></p>
     <p>Здравствуйте <font color='red'> <?php echo htmlspecialchars($_POST['Имя']); ?> </font></p>
     <p>Ваш e-mail: <font color='red'> <?php echo htmlspecialchars($_POST['Мэйл']); ?> </font></p>
-    <p>Ваша доставка:</p>
-    <p>Форма посылки: <font color='red'> <?php echo htmlspecialchars($_POST['forma']); ?> </font></p>
+    <p>Ваша доставка: <font color='red'> <br>
+      <?php
+        if($_POST['choise'] != 0)
+        {
+          foreach($_POST['choise'] as $choise)
+          {
+            if($choise == 'f1')
+              echo "Курьер <br/>";
+            if($choise == 'f2')
+              echo "Самолет <br/>";
+            if($choise == 'f3')
+              echo "Поезд <br/>";
+            if($choise == 'f4')
+              echo "Автотранспорт <br/>";
+          }
+        }
+      ?>
+    </font></p>
+    <p>Форма посылки: <font color='red'>
+      <?php
+        if($_POST['forma'] == 's1')
+          echo "Прямоугольная <br/>";
+        if($_POST['forma'] == 's2')
+          echo "Квадратная <br/>";
+        if($_POST['forma'] == 's3')
+          echo "Цилиндрическая <br/>";
+      ?>
+    </font></p>
     <p>Количество: <font color='red'> <?php echo htmlspecialchars($_POST['number']); ?> </font></p>
-    <p>ВЕС посылки:<font color='red'>
+    <p>ВЕС посылки:<font color="red">
       <?php
         if(htmlspecialchars($_POST['mass']) == 'r1')
         {
@@ -27,26 +53,30 @@
         }
       ?>
     </font></p>
-    <p>Цвет посылки: </p>
-    <p>КОД ЦВЕТА= <?php echo htmlspecialchars($_POST['Цвет']); ?></p>
-    <p>Была сипользована тара:
+    <?php
+    $qwe = htmlspecialchars($_POST['Цвет']);
+    echo "<p>Цвет посылки: <font color=".$qwe."> <table bgcolor=".$qwe."><tr><td></td></tr></table></font> </p>";
+    echo "<p> <font color=".$qwe."> КОД ЦВЕТА= $qwe </font> </p>";
+    ?>
+    <p>Была сипользована тара: <font color='red'>
     <br>
     <?php
-      $mass = array('t1' => 0, 't2' => 0, 't3' => 0, 't4' => 0);
-      if (htmlspecialchars($_POST['tara']) == 't1')
-        $mass['t1'] = 1;
-      if (htmlspecialchars($_POST['tara']) == 't2')
-        $mass['t2'] = 1;
-      if (htmlspecialchars($_POST['tara']) == 't3')
-        $mass['t3'] = 1;
-      if (htmlspecialchars($_POST['tara']) == 't4')
-        $mass['t4'] = 1;
-      foreach($mass as $tara)
+      if($_POST['tara'] != 0)
       {
-        echo $tara;
+        foreach($_POST['tara'] as $tara)
+        {
+          if($tara == 't1')
+            echo "Бьющаяся <br/>";
+          if($tara == 't2')
+            echo "Хрупкая <br/>";
+          if($tara == 't3')
+            echo "Водонепроницаемая <br/>";
+          if($tara == 't4')
+            echo "Пожароопасная <br/>";
+        }
       }
     ?>
-    </p>
+    </font></p>
     <p>КОММЕНТАРИЙ: <font color='red'> <?php echo htmlspecialchars($_POST['comment']); ?> </font></p>
   </body>
 </html>
